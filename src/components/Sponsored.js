4222a59fd19a8817c43d5113aa8f1fe7c80d10a3
Joshua <joshuaj181992@gmail.com>
2025-11-21 19:54:53 -0500

    Trigger redeploy

--- src/components/Sponsored.js
+++ src/components/Sponsored.js
@@ -28,7 +28,7 @@ const sponsors = [
   },
   {
   name: "DSS",
-  img: "/images/sp5.png",
+  img: "/images/Untitled-6.png",
   url: "https://www.tiktok.com/@dragonshellshinobi?_r=1&_t=ZP-91b9cC5sXtf", // <-- put the real DSS link here
   tagline: "Performance PCs for Pros"
 }
