102f56d00e53664d45fa554ac7f589730a2ae5d5
Joshua <joshuaj181992@gmail.com>
2025-11-21 20:01:13 -0500

    Trigger redeploy

--- src/components/Sponsored.js
+++ src/components/Sponsored.js
@@ -28,7 +28,7 @@ const sponsors = [
   },
   {
   name: "DSS",
-    img: "/images/sp6.png",
+    img: "/images/sp7.png",
   url: "https://www.tiktok.com/@dragonshellshinobi?_r=1&_t=ZP-91b9cC5sXtf", // <-- put the real DSS link here
   tagline: "Dragon Shield Shinbo"
 }
